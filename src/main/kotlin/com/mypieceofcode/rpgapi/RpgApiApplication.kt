9431fa9a18e1339b2ea1ee3a1aef722fe8ce10cb
dominik <jurasz.do@gmail.com>
2018-10-27 16:58:17 +0200

    initial commit

---
+++ src/main/kotlin/com/mypieceofcode/rpgapi/RpgApiApplication.kt
@@ -0,0 +1,11 @@
+package com.mypieceofcode.rpgapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RpgApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<RpgApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/mypieceofcode/rpgapi/RpgApiApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.mypieceofcode.rpgapi
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class RpgApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
